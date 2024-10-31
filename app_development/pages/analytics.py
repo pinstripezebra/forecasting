0a12eb76d08b0ff4ea874b815e72f6fcc9cfdb03
pinstripezebra <seelcs12@gmail.com>
2024-10-31 12:04:43 -0700

    heatmap working

--- app_development/pages/analytics.py
+++ app_development/pages/analytics.py
@@ -1,10 +1,11 @@
 import dash
 from dash import html, dcc, callback, Input, Output
 
+
 dash.register_page(__name__)
 
 layout = html.Div([
-    html.H1('This is our Analytics page'),
+    html.H1('This is our Geographic page'),
     html.Div([
         "Select a city: ",
         dcc.RadioItems(
