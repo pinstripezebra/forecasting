2b3e7d306f5c90ac8341b6c48eb0f2518e50dde6
pinstripezebra <seelcs12@gmail.com>
2024-11-29 08:00:06 -0800

    chatbot demo working

--- app_development/llm/test_llm.py
+++ app_development/llm/test_llm.py
@@ -1,21 +1,42 @@
 import os
-from huggingface_hub import login, InferenceClient
 from dotenv import find_dotenv, load_dotenv
+
+import getpass
+from langchain_anthropic import ChatAnthropic
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+import anthropic
 
 # returning api key and logging in
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
-key = os.getenv("HUGGING_FACE_API_KEY")
-print(key)
-login(key)
+api_key = os.getenv("ANTHROPIC_API_KEY")
 
-client = InferenceClient(model="meta-llama/Meta-Llama-3-70B-Instruct")
+client = anthropic.Anthropic(
+    # defaults to os.environ.get("ANTHROPIC_API_KEY")
+    api_key=api_key,
+)
 
-def llm_engine(messages, stop_sequences=["Task"]) -> str:
-    response = client.chat_completion(messages, stop=stop_sequences, max_tokens=1000)
-    answer = response.choices[0].message.content
-    return answer
+test_weather = [30, 20]
+# Defining function
+def return_output(input):
+    
+    degrees = input[0]
+    wind = input[1]
 
-test_input = "what is the capital of france?"
+    client = anthropic.Anthropic(
+        api_key=api_key,
+    )
+    message = client.messages.create(
+    model="claude-3-haiku-20240307",
+    max_tokens=1000,
+    temperature=0.0,
+    system="you are a helpful meteorologist assisstant, you have an in depth understanding of weather forecasting and are trying to help users understand upcoming weather.",
+    messages=[
+        {"role": "user", "content": "If the forecasted temperature is {degrees} farenheight with {wind} mph wind, what will running in this be like?".format(degrees = degrees, wind = wind)}
+    ]
+)
+    return message.content
 
-print(llm_engine(test_input))
+print(return_output(test_weather))
+
