5941296b1515738488acc3705439032a4eef7f4c
pinstripezebra <seelcs12@gmail.com>
2024-12-12 06:48:44 -0800

    updating base app file to not use user authentication for testing purposes, will have separate file with user authentication

--- app_development/app.py
+++ app_development/app.py
@@ -22,9 +22,6 @@ load_dotenv(dotenv_path)
 LATITUDE, LONGITUDE = float(os.getenv("LATITUDE")), float(os.getenv("LONGITUDE"))
 repull_data = True
 
-# authentication
-username, password = os.getenv("ADMIN_USERNAME"), os.getenv("ADMIN_PASSWORD")
-db = os.getenv("SERVER")
 
 # defining path
 parent_path = os.path.dirname(os.path.dirname(__file__))
@@ -43,61 +40,6 @@ with open(parent_path + '/app_development/style/content_style.json') as f:
 # defining and Initializing the app
 server = flask.Flask(__name__)
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP], assets_folder='assets', assets_url_path='/assets/', server = server)
-#auth = dash_auth.BasicAuth(app,{username:password})
-
-# Updating the Flask Server configuration with Secret Key to encrypt the user session cookie
-server.config.update(SECRET_KEY=os.getenv('SECRET_KEY'))
-
-# Login manager object will be used to login / logout users
-login_manager = LoginManager()
-login_manager.init_app(server)
-login_manager.login_view = '/login'
-
-# User data model. It has to have at least self.id as a minimum
-
-class User(UserMixin):
-    def __init__(self, username):
-        self.id = username
-
-@ login_manager.user_loader
-def load_user(username):
-    ''' This function loads the user by user id. Typically this looks up the user from a user database.
-        We won't be registering or looking up users in this example, since we'll just login using LDAP server.
-        So we'll simply return a User object with the passed in username.
-    '''
-    return User(username)
-# Login screen
-login = html.Div([dcc.Location(id='url_login', refresh=True),
-                  html.H2('''Please log in to continue:''', id='h1'),
-                  dcc.Input(placeholder='Enter your username',
-                            type='text', id='uname-box'),
-                  dcc.Input(placeholder='Enter your password',
-                            type='password', id='pwd-box'),
-                  html.Button(children='Login', n_clicks=0,
-                              type='submit', id='login-button'),
-                  html.Div(children='', id='output-state'),
-                  html.Br(),
-                  dcc.Link('Home', href='/')])
-
-# Successful login
-success = html.Div([html.Div([html.H2('Login successful.'),
-                              html.Br(),
-                              dcc.Link('Home', href='/')])  # end div
-                    ])  # end div
-
-# Failed Login
-failed = html.Div([html.Div([html.H2('Log in Failed. Please try again.'),
-                             html.Br(),
-                             html.Div([login]),
-                             dcc.Link('Home', href='/')
-                             ])  # end div
-                   ])  # end div
-
-# logout
-logout = html.Div([html.Div(html.H2('You have been logged out - Please login')),
-                   html.Br(),
-                   dcc.Link('Home', href='/')
-                   ])  # end div
 
 # Defining components
 sidebar = html.Div(children = [
@@ -144,92 +86,15 @@ sidebar = html.Div(children = [
         ], style=SIDEBAR_STYLE
     )
 
-'''
+
 app.layout = html.Div([sidebar,
     html.Div([
             dash.page_container
     ], style=CONTENT_STYLE)
 ])
-'''
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    dcc.Location(id='redirect', refresh=True),
-    dcc.Store(id='login-status', storage_type='session'),
-    html.Div(id='user-status-div'),
-    html.Br(),
-    html.Hr(),
-    html.Br(),
-    html.Div(id='page-content'), 
-    html.Div([sidebar,
-    html.Div([
-            dash.page_container
-    ], style=CONTENT_STYLE)
-]),
-])
-page_1_layout = html.Div([html.H1('Page 1')])
-page_2_layout = html.Div([html.H1('Page 2')])
-index_page = html.Div([html.H1('index page')])
-
-# Callback function to login the user, or update the screen if the username or password are incorrect
 
 
-@app.callback(
-    [Output('url_login', 'pathname'), Output('output-state', 'children')], [Input('login-button', 'n_clicks')], [State('uname-box', 'value'), State('pwd-box', 'value')])
-def login_button_click(n_clicks, username, password):
-    if n_clicks > 0:
-        if username == 'test' and password == 'test':
-            user = User(username)
-            login_user(user)
-            return '/success', ''
-        else:
-            return '/login', 'Incorrect username or password'
-
-    return dash.no_update, dash.no_update  # Return a placeholder to indicate no update
-
-# Main router
-
-@app.callback(Output('page-content', 'children'), Output('redirect', 'pathname'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    ''' callback to determine layout to return '''
-    # We need to determine two things for everytime the user navigates:
-    # Can they access this page? If so, we just return the view
-    # Otherwise, if they need to be authenticated first, we need to redirect them to the login page
-    # So we have two outputs, the first is which view we'll return
-    # The second one is a redirection to another page is needed
-    # In most cases, we won't need to redirect. Instead of having to return two variables everytime in the if statement
-    # We setup the defaults at the beginning, with redirect to dash.no_update; which simply means, just keep the requested url
-    view = None
-    url = dash.no_update
-    if pathname == '/login':
-        view = login
-    elif pathname == '/success':
-        if current_user.is_authenticated:
-            view = success
-        else:
-            view = failed
-    elif pathname == '/logout':
-        if current_user.is_authenticated:
-            logout_user()
-            view = logout
-        else:
-            view = login
-            url = '/login'
-
-    elif pathname == '/page-1':
-        view = page_1_layout
-    elif pathname == '/page-2':
-        if current_user.is_authenticated:
-            view = page_2_layout
-        else:
-            view = 'Redirecting to login...'
-            url = '/login'
-    else:
-        view = index_page
-    # You could also return a 404 "URL not found" page here
-    return view, url
 
 # Running the app
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
