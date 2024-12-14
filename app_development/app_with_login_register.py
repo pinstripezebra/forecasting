e4d72064cfef69b456d4bebd00df61e716f3166b
pinstripezebra <seelcs12@gmail.com>
2024-12-14 11:30:45 -0800

    cleaning up files

--- app_development/app_with_login_register.py
+++ app_development/app_with_login_register.py
@@ -9,11 +9,10 @@ import os
 import numpy as np
 from dotenv import find_dotenv, load_dotenv
 import json
-from utility.data_query import data_pipeline
+from utility.data_query import data_pipeline, retrieve_users
 import dash_auth
 import flask
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
-
 
 
 # loading environmental variables
@@ -23,7 +22,7 @@ LATITUDE, LONGITUDE = float(os.getenv("LATITUDE")), float(os.getenv("LONGITUDE")
 repull_data = True
 
 # authentication
-test_username, test_password = os.getenv("ADMIN_USERNAME"), os.getenv("ADMIN_PASSWORD")
+users = retrieve_users()
 
 # defining path
 parent_path = os.path.dirname(os.path.dirname(__file__))
@@ -41,7 +40,7 @@ with open(parent_path + '/app_development/style/content_style.json') as f:
 
 # defining and Initializing the app
 server = flask.Flask(__name__)
-app = Dash(__name__,  external_stylesheets=[dbc.themes.BOOTSTRAP], assets_folder='assets', assets_url_path='/assets/', server = server)
+app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP], assets_folder='assets', assets_url_path='/assets/', server = server)
 #auth = dash_auth.BasicAuth(app,{username:password})
 username, password = os.getenv("ADMIN_USERNAME"), os.getenv("ADMIN_PASSWORD")
 # Updating the Flask Server configuration with Secret Key to encrypt the user session cookie
@@ -65,6 +64,7 @@ def load_user(username):
         So we'll simply return a User object with the passed in username.
     '''
     return User(username)
+
 # Login screen
 login = html.Div([dcc.Location(id='url_login', refresh=True),
                   html.H2('''Please log in to continue:''', id='h1'),
@@ -75,28 +75,27 @@ login = html.Div([dcc.Location(id='url_login', refresh=True),
                   html.Button(children='Login', n_clicks=0,
                               type='submit', id='login-button'),
                   html.Div(children='', id='output-state'),
-                  html.Br(),
-                  dcc.Link('Home', href='/')])
+                  html.Br()])
 
-# Successful login
-success = html.Div([html.Div([html.H2('Login successful.'),
-                              html.Br(),
-                              dcc.Link('Home', href='/')])  # end div
-                    ])  # end div
 
 # Failed Login
 failed = html.Div([html.Div([html.H2('Log in Failed. Please try again.'),
                              html.Br(),
                              html.Div([login]),
                              dcc.Link('Home', href='/')
-                             ])  # end div
-                   ])  # end div
+                             ])  
+                   ])  
 
 # logout
 logout = html.Div([html.Div(html.H2('You have been logged out - Please login')),
                    html.Br(),
+                   ])  
+
+# logout
+error404 = html.Div([html.Div(html.H2('Error 404 - page not found')),
+                   html.Br(),
                    dcc.Link('Home', href='/')
-                   ])  # end div
+                   ])  
 
 
 app.layout = html.Div([
@@ -110,51 +109,70 @@ app.layout = html.Div([
     html.Div(id='page-content'),
 ])
 
+print([page["relative_path"] for page in dash.page_registry.values()])
+sidebar = html.Div(children = [
+            html.Img(
+                        alt="Link to Github",
+                        src="./assets/app_logo.png",
+                        style={'height':'10%', 'width':'40%', 'margin': 'auto'}
+                    ),
+            html.H3("Pages"),
+            html.Hr(),
+            html.Div([   
+                dbc.Nav([
+                    dbc.NavLink(f"{page['name']}", href = page["relative_path"]) for page in dash.page_registry.values() if page["relative_path"] != '/register'
+                ], vertical=True)
 
-index_page = html.Div([
-    dcc.Link('Go to Page 1', href='/page-1'),
-    html.Br(),
-    dcc.Link('Go to Page 2', href='/page-2'),
-])
-page_1_layout = html.Div([
-    html.H1('Page 1'),
-    dcc.Dropdown(
-        id='page-1-dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='page-1-content'),
-    html.Br(),
-    dcc.Link('Go to Page 2', href='/page-2'),
-    html.Br(),
-    dcc.Link('Go back to home', href='/'),
-])
-page_2_layout = html.Div([
-    html.H1('Page 2'),
-    dcc.RadioItems(
-        id='page-2-radios',
-        options=[{'label': i, 'value': i} for i in ['Orange', 'Blue', 'Red']],
-        value='Orange'
-    ),
-    html.Div(id='page-2-content'),
-    html.Br(),
-    dcc.Link('Go to Page 1', href='/page-1'),
-    html.Br(),
-    dcc.Link('Go back to home', href='/')
-])
-index_page = html.Div([html.H1('index page')])
+            ]),
+            html.H3("Login"),
+            html.Div([
+                dcc.Link('Logout', href='/logout'),
+            ]),
+            html.H3("Description"),
+            html.P(
+                "Your custom running companion allowing you to plan out your perfect time to run ensuring you never miss a workout.", className="text"
+            ),
+            html.H3("Model"
+            ),
+            html.P(
+                "This project uses machine learning to forecast running conditions and provides a personalized running reccommendation time based on user preferences.", className="text"
+            ),
+
+            html.H3("Code"
+            ),
+            html.P(
+                "The complete code for this project is available on github.", className="text"
+            ),
+            html.A(
+                href="https://github.com/pinstripezebra/Dash-Tutorial",
+                children=[
+                    html.Img(
+                        alt="Link to Github",
+                        src="./assets/github_logo.png",
+                        style={'height':'3%', 'width':'8%'}
+                    )
+                ],
+                style = {'color':'black'}
+            )
+
+        ], style=SIDEBAR_STYLE
+    )
+home_page = html.Div([sidebar,
+        html.Div([
+                dash.page_container
+        ], style=CONTENT_STYLE)
+    ])
 
 # Callback function to login the user, or update the screen if the username or password are incorrect
-
-
 @callback(
     [Output('url_login', 'pathname'), Output('output-state', 'children')], [Input('login-button', 'n_clicks')], [State('uname-box', 'value'), State('pwd-box', 'value')])
 def login_button_click(n_clicks, username, password):
     if n_clicks > 0:
-        if username == test_username and password == test_password:
+        if username in users['username'].to_list() and password == users[users['username']== username]['password'].values:
             user = User(username)
             login_user(user)
-            return '/success', ''
+            # navigate to landing page if logged in successfully 
+            return '/landing', ''
         else:
             return '/login', 'Incorrect username or password'
 
@@ -175,11 +193,12 @@ def display_page(pathname):
     # We setup the defaults at the beginning, with redirect to dash.no_update; which simply means, just keep the requested url
     view = None
     url = dash.no_update
+
     if pathname == '/login':
         view = login
     elif pathname == '/success':
         if current_user.is_authenticated:
-            view = success
+            view = home_page
         else:
             view = failed
     elif pathname == '/logout':
@@ -190,17 +209,19 @@ def display_page(pathname):
             view = login
             url = '/login'
 
-    elif pathname == '/page-1':
-        view = page_1_layout
-    elif pathname == '/page-2':
+    
+    # if we're logged in and want to view one of the pages
+    elif pathname == '/analytic' or pathname == '/landing' or pathname == '/map':
         if current_user.is_authenticated:
-            view = page_2_layout
+            view = home_page
         else:
             view = 'Redirecting to login...'
             url = '/login'
+
+
+    #else:
     else:
-        view = index_page
-    # You could also return a 404 "URL not found" page here
+        view = error404
     return view, url
 
 @callback(Output('user-status-div', 'children'), Output('login-status', 'data'), [Input('url', 'pathname')])
