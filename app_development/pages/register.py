936ed4b05cd979789e47b979d6c308957224c4cc
pinstripezebra <seelcs12@gmail.com>
2024-12-21 13:58:36 -0800

    registration generating successful registration

--- app_development/pages/register.py
+++ app_development/pages/register.py
@@ -51,7 +51,8 @@ layout = html.Div([
                     html.Br(),
                     dbc.Button('Register', n_clicks=0,className="me-2", id='Register-button'),
 
-                ],style = {'align-items':'center', 'justify-content':'center', })
+                ],style = {'align-items':'center', 'justify-content':'center', }),
+                html.Div(id = 'registration-message')
             ])
         ], className='text-center', style={"width": "25rem", 'background-color': 'rgba(245, 245, 245, 1)', 'opacity': '.8'}),
         width={"offset": 4},
@@ -61,12 +62,14 @@ layout = html.Div([
 # Callback for registering user
 @callback(
     Output("password-error", "children"),
+    Output("registration-message", "children"),
     Input("Register-button", "n_clicks"),
     Input("register-uname-box", "value"),
     Input("register-email-box", "value"),
     Input("register-pwd-box", "value"),
     Input("register-pwd-box2", "value"),
-    Input('address-box', "value")
+    Input('address-box', "value"),
+    prevent_initial_call = True
 )
 def register_user_to_database(n_clicks, username, email, password1, password2, address):
     
@@ -82,12 +85,56 @@ def register_user_to_database(n_clicks, username, email, password1, password2, a
             print(registration_error)
             if registration_error == "no error":
                 insert_user(username, password1, str(latitude), str(longitude))
+                return html.Div([html.P(registration_error)]), html.Div([html.H3('Successfully Registered!')])
         else:
             registration_error = "passwords must match"
         
-    if registration_error == 'no error':
-        registration_error = ""
-    return html.Div([html.P(registration_error)])
+    #if registration_error == 'no error':
+    #    registration_error = ""
+    return html.Div([html.P(registration_error)]),  html.Div([])
+'''
+@callback(
+Output("Intake-Form", 'children'),
+Input("password-error-text", "text")
+)
 
+def intake_form_status(register_error):
 
-    
+    # if we've registered successfuly
+    if register_error =="no error":
+        print("here")
+        return html.Div([
+            html.H3('Registered Successfully!')
+        ])
+
+    else:
+        return html.Div([
+                    html.Img(
+                        alt="Link to Github",
+                        src="./assets/logo.png",
+                        style={'height':'3%', 'width':'16%', 'margin': 'auto', "opacity": '0.8','display': 'inline'}
+                    ),
+                    html.H3('Optirun', style={'display': 'inline' }),
+                    html.H1('Account Creation'),
+                    html.H5('Username'),
+                    dcc.Input(placeholder='Enter your username',
+                                            type='text', id='register-uname-box'),
+                    html.H5('Email'),
+                    dcc.Input(placeholder='Enter your email',
+                                            type='text', id='register-email-box'),
+                    html.H5('Password'),
+                    dcc.Input(placeholder='Enter your password',
+                                type='password', id='register-pwd-box'),
+                    html.H5('Confirm Password'),
+                    dcc.Input(placeholder='Confirm your password',
+                                type='password', id='register-pwd-box2'),
+                    html.Div(id="password-error"),
+                    html.H5('Address'),
+                    dcc.Input(placeholder='Enter your Address',
+                                type='text', id='address-box'),
+                    html.Br(),
+                    html.Br(),
+                    dbc.Button('Register', n_clicks=0,className="me-2", id='Register-button'),
+
+                ],style = {'align-items':'center', 'justify-content':'center', })
+'''
